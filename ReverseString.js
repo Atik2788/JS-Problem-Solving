a3ca3c75465c8975d61cf30799dab9f9f67d2134
Atik2788 <ashanto653.2@gmail.com>
2024-10-03 14:59:32 +0600

    change

--- ReverseString.js
+++ ReverseString.js
@@ -1,6 +1,6 @@
 function reverseString (str) {
-    return str.split("").reverse().join("");
-    // return str.split("").reverse().join("")
+    return str.split("").reverse().join("")
+    str.split("").reverse().join("");
 }
 
-console.log(reverseString('hello'))
+console.log(reverseString("hello worlde"))
