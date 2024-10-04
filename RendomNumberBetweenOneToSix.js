bf626737fa811139680e939b99284f5076ab39c2
Atik2788 <ashanto653.2@gmail.com>
2024-10-04 19:17:39 +0600

    after adding check even or odd

--- RendomNumberBetweenOneToSix.js
+++ RendomNumberBetweenOneToSix.js
@@ -1,3 +1,11 @@
+//get rendom number
+function getRendomNumber(min, max){
+    
+}
+console.log(getRendomNumber(1,6)); // answer will be: 1,6,2,4
+
+
+
 function getRendomNumber(min, max){
     return Math.floor(Math.random() * (max-min + 1)  + min)
 }
