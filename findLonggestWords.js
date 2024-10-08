298b6a9d82521a422d23a49ce54bfe9e65d4c280
Atik2788 <ashanto653.2@gmail.com>
2024-10-08 14:08:24 +0600

    remvoeDuplicates

--- findLonggestWords.js
+++ findLonggestWords.js
@@ -1,6 +1,14 @@
-function longestWords (sentence){
+function longestWord(sentence) {
     let words = sentence.split(" ");
     let longest = "";
 
-    
+    for (let word of words) {
+        if (word.length > longest.length) {
+            longest = word;
+        }
+    }
+
+    return longest;
 }
+
+console.log(longestWord("The quick brown fox jumps over the lazy dog")); // Output: "jumps"
