261d65205d44925a30cac0802cfa6e4b59d3e63c
Atik2788 <ashanto653.2@gmail.com>
2024-10-10 06:22:25 +0600

    sort array descending

--- sortArray.js
+++ sortArray.js
@@ -1,5 +1,11 @@
+// in ascending
 function sortArray(arr){
     return arr.sort((a,b) => a-b)
 }
-
 console.log(sortArray([5,9,6,5,6,7,25,69,87,45,5,19]));
+
+// in descending
+function sortArrayDes(arr){
+    return arr.sort((a,b) => b-a)
+}
+console.log(sortArrayDes([5,9,6,5,6,7,25,69,87,45,5,19]));
