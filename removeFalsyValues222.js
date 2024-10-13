12d43accda9c4641bd20be46d6acd97df1d1a197
Md. Atikur Rahman Shanta <ashanto653.2@gmail.com>
2024-10-13 01:00:55 +0000

    Remove falsy values from an array

--- removeFalsyValues222.js
+++ removeFalsyValues222.js
@@ -3,3 +3,8 @@ function removeFalsyValue(arr){
 }
 
 console.log(removeFalsyValue([0, 1, 2, '', 3, false, 4]))
+function removeFalsyValue(arr){
+ return arr.filter(Boolean);
+}
+
+console.log(removeFalsyValue([0, 1, 2, ' ', 3, false, 4]))
