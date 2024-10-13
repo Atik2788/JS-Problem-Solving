cf3e75ea49b751e175ee5ffc0fde30e2bedcecb7
Md. Atikur Rahman Shanta <107786516+Atik2788@users.noreply.github.com>
2024-10-13 08:20:33 +0600

    Update removeFalsyValues222.js

--- removeFalsyValues222.js
+++ removeFalsyValues222.js
@@ -2,9 +2,4 @@ function removeFalsyValue(arr){
  return arr.filter(Boolean);
 }
 
-console.log(removeFalsyValue([0, 1, 2, '', 3, false, 4]))
-function removeFalsyValue(arr){
- return arr.filter(Boolean);
-}
-
 console.log(removeFalsyValue([0, 1, 2, ' ', 3, false, 4]))
