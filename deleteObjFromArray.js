aa5b16fc138ffc5248472ff3a295e9dffa46d434
Atik2788 <ashanto653.2@gmail.com>
2024-10-18 21:19:02 +0600

    delete obj wihichs id is 4

--- deleteObjFromArray.js
+++ deleteObjFromArray.js
@@ -6,10 +6,16 @@ const arr = [
 	{ id: 4, value: 40 },
 	{ id: 5, value: 50 },
 ];
-console.log(arr);
 
-const index = arr.findIndex((item) =>item.id === 4)
-const arr1 = arr.splice(index, 1)
-console.log(arr1);
-console.log(arr);
+// Way 1 findIndex
+// console.log(arr);
+// const index = arr.findIndex((item) =>item.id === 4)
+// const arr1 = arr.splice(index, 1)
+// console.log(arr1);
+// console.log(arr);
 
+
+// Way 2 with filter
+const arr2 = arr.filter((item) => item.id !== 4)
+console.log(arr);
+console.log(arr2);
