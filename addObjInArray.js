f982294ba75e889c913bcef72439627a83a6775c
Atik2788 <ashanto653.2@gmail.com>
2024-10-20 21:53:29 +0600

    addObjInArray

--- addObjInArray.js
+++ addObjInArray.js
@@ -1,9 +1,9 @@
 // If you want to add a new object at the end of the array:
-let users = [
+const users = [
     { id: 1, name: 'John' },
     { id: 2, name: 'Jane' }
 ];
-let newuser = {id: 3, name: "David"}
+const newUser = {id: 3, name: "David"}
 
-users.push(newuser)
+users.push(newUser)
 console.log(users);
