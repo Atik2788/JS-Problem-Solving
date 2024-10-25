71b1e2fe5634a8759c9c12a3d23e624b77491cb3
Atik2788 <ashanto653.2@gmail.com>
2024-10-25 20:39:54 +0600

    reduce multiply of array values.

--- useReduce.js
+++ useReduce.js
@@ -4,11 +4,16 @@ function findLargestReduse(numbers){
     return max;
 }
 
-console.log(findLargestReduse([12, 2, 58, 66, 99, 88, 99, 115, 225]));
+console.log("find largest:", findLargestReduse([12, 2, 58, 66, 99, 88, 99, 115, 225]));
 
 // Sum of numbers in Array
 function sumOfNumbers (arr){
     const sum = arr.reduce((total, num) => total + num, 0)
     return sum
 }
-console.log(sumOfNumbers([12, 2, 58, 66, 99, 88, 99, 115, 225, 1]));
+console.log("sum of Numbers:", sumOfNumbers([12, 2, 58, 66, 99, 88, 99, 115, 225, 1]));
+
+// Multiplies of numbers in Array
+const numbers = [12, 2, 58, 66, 1]
+const multiplies = numbers.reduce((acc, cur) => acc * cur, 1)
+console.log("multiplies:", multiplies);
