bfb837f8a398ab0c77f73a6c1e435dd7ffe99530
Atik2788 <ashanto653.2@gmail.com>
2024-10-25 20:32:32 +0600

    find largest with reduce

--- FindLargest.js
+++ FindLargest.js
@@ -3,3 +3,13 @@ function findLargest (arr) {
 }
 
 console.log(findLargest([12, 2, 58, 66, 99, 88, 99, 115]));
+
+
+// using >>> Reduce <<<
+function findLargestReduse(numbers){
+    const max = numbers.reduce((num, acc) => (num > acc ? num : acc), numbers[0])
+    return max;
+}
+
+console.log(findLargestReduse([12, 2, 58, 66, 99, 88, 99, 115, 225]));
+
