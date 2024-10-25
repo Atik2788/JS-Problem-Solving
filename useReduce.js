e8c8bd55d7864a5275f4efef73cfad27a1c93d9f
Atik2788 <ashanto653.2@gmail.com>
2024-10-25 20:46:04 +0600

    reduce Find UniqueNumbers

--- useReduce.js
+++ useReduce.js
@@ -17,3 +17,11 @@ console.log("sum of Numbers:", sumOfNumbers([12, 2, 58, 66, 99, 88, 99, 115, 225
 const numbers = [12, 2, 58, 66, 1]
 const multiplies = numbers.reduce((acc, cur) => acc * cur, 1)
 console.log("multiplies:", multiplies);
+
+// Find UniqueNumbers 
+const num2 = [1, 2, 2, 3, 6, 6, 5, 9, 9]
+const uniqueNumbers = num2.reduce((acc, cur) =>{
+    if(!acc.includes(cur)) acc.push(cur)
+        return acc;
+}, [])
+console.log("unique numbers: ",uniqueNumbers);
