70a67e6ffa2f00ebff7400f62a4bf4246ff6d62f
Atik2788 <ashanto653.2@gmail.com>
2024-10-26 17:05:02 +0600

    fetch all data

--- fetchDataFromAnArray.js
+++ fetchDataFromAnArray.js
@@ -22,3 +22,10 @@ const users = [
 const firstUser = getFirstElementByCondition(users, user => user.age >=25)    
 
 console.log(firstUser)
+
+// fetch all value
+const users2 = [
+    { id: 1, name: "Alice", age: 25 },
+    { id: 2, name: "Bob", age: 30 },
+    { id: 3, name: "Charlie", age: 35 }
+];
