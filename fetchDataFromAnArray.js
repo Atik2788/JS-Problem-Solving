b02a9056dc8995ec51e7953ae99141cad6a931bd
Atik2788 <ashanto653.2@gmail.com>
2024-10-26 16:57:29 +0600

    You can use the filter() method to retrieve all elements that meet a certain condition.

--- fetchDataFromAnArray.js
+++ fetchDataFromAnArray.js
@@ -5,4 +5,20 @@ function getElementsByCondition (arr, condition){
 
 const ages = [15, 18, 21, 16, 30];
 const adults = getElementsByCondition(ages, age => age >= 18)
-console.log(adults)
+console.log(adults) // [18, 21, 30]
+
+
+// You can use the filter() method to get the first element that matches a specific condition.
+function getFirstElementByCondition (arr, condition){
+    return arr.filter(condition)
+}
+
+const users = [
+    { id: 1, name: "Alice", age: 25 },
+    { id: 2, name: "Bob", age: 30 },
+    { id: 3, name: "Charlie", age: 35 }
+];
+
+const firstUser = getFirstElementByCondition(users, user => user.age >=25)    
+
+console.log(firstUser)
